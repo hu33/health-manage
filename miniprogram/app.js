a44f36f16f5068828253e4a07ba32f3474c9b349
hu33 <784376977@qq.com>
2019-07-13 10:29:32 +0800

    添加自动更新新版本

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -18,6 +18,38 @@ App({
         this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
       }
     })
+  },
+  onShow: function(){
+    // 启动自动更新
+    if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager();
+      updateManager.onCheckForUpdate(function (res) {
+        if (res.hasUpdate) { // 请求完新版本信息的回调
+          updateManager.onUpdateReady(function () {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              success: function (res) {
+                if (res.confirm) {// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                  updateManager.applyUpdate()
+                }
+              }
+            })
+          });
+          updateManager.onUpdateFailed(function () {
+            wx.showModal({// 新的版本下载失败
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索进入哟~',
+            })
+          })
+        }
+      })
+    } else {
+      wx.showModal({// 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
   },
   globalData: {
     ColorList: [{
